9b36108441f73b513c01f080168214a5d1e95d43
LorenzoVarese <lorenzovarese@users.noreply.github.com>
2023-12-23 21:08:16 +0100

    Basics of Composition API with a simple counter

--- TutorialEnvatoTuts/vue-start-spa/src/views/PagesList.vue
+++ TutorialEnvatoTuts/vue-start-spa/src/views/PagesList.vue
@@ -1,3 +1,39 @@
 <template>
-    <p>this is the page list</p>
+    <p>{{ counter }}</p> <!--Second option requires data.counter-->
+    <button class="btn btn-primary" @click.prevent="increment">Increment</button>
 </template>
+
+<script setup>
+// After the composition API
+import { reactive, ref } from 'vue';
+
+// First option
+let counter = ref(0); // {value: 0}
+
+function increment() {
+    counter.value++;
+}
+
+// Second option
+// const data = reactive({
+//     counter: 0
+// });
+
+// function increment() {
+//     data.counter++;
+// }
+
+// Before the composition API
+// export default {
+//     data() {
+//         return {
+//             counter: 0
+//         }
+//     },
+//     methods: {
+//         increment() {
+//             this.counter++;
+//         }
+//     }
+// }
+</script>

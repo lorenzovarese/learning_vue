99cfcde787e2136455086744cc6c87dbd838e6a7
LorenzoVarese <lorenzovarese@users.noreply.github.com>
2023-12-23 11:08:21 +0100

    Create first single page application starting from the old index.html

---
+++ TutorialEnvatoTuts/vue-start-spa/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <navbar 
+        :pages="pages" 
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index">
+    </navbar>
+    <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue'
+import Navbar from './components/Navbar.vue';
+
+export default {
+    components: {
+        PageViewer,
+        Navbar
+    },
+    data() {
+        return {
+            activePage: 0,
+            pages: [
+                {
+                    link: { text: 'Home', url: 'index.html' },
+                    pageTitle: 'Hello, Gigi!',
+                    content: 'Welcome to the magic world of Vue!'
+                },
+                {
+                    link: { text: 'About', url: 'about.html' },
+                    pageTitle: 'About',
+                    content: 'This is the about page'
+                },
+                {
+                    link: { text: 'Contact', url: 'contact.html' },
+                    pageTitle: 'Contact',
+                    content: 'This is the contact page'
+                }
+            ]
+        };
+    }
+}
+</script>
---
+++ TutorialEnvatoTuts/vue-start-spa/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar navbar-expand-lg" :class="[`navbar-${theme}`, `bg-${theme}`, navbar, navbar - expand - lg]">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Vue</a>
+            <ul class="navbar-nav">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <!-- v-bind:href=page.url is equal to :href=page.url-->
+                    <a class="nav-link" :class="{ active: activePage === index }" aria-current="page" :href=page.link.url
+                        :title="`This title goes to the ${page.link.text} page`" v-on:click.prevent="navLinkClick(index)">
+                        <!-- v-on:click is equal to @click -->
+                        <!-- v-on:click.prevent prevent the move tho href link-->
+                        <!-- to use string interpolation, we need to use backticks -->
+                        {{ page.link.text }}
+                    </a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-primary" @click.prevent="changeTheme()">Toggle</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: ['pages', 'activePage', 'theme', 'navLinkClick'],
+    data() {
+        return {
+            theme: 'light'
+        }
+    },
+    methods: {
+        changeTheme() {
+            this.theme = this.theme === 'dark' ? 'light' : 'dark';
+        }
+    }
+}
+</script>
